b27b86a82b6f07446c7691e082e5f7ea51a9e895
Vivek-k24 <vivek.kallakuri@gmail.com>
2026-07-17 14:15:09 -0500

    Configure GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,2 @@
-import React from 'react';import ReactDOM from 'react-dom/client';import {BrowserRouter} from 'react-router-dom';import App from './app/App';import {AppProvider} from './state/AppState';import './styles/index.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><AppProvider><App/></AppProvider></BrowserRouter></React.StrictMode>);
+import React from 'react';import ReactDOM from 'react-dom/client';import {HashRouter} from 'react-router-dom';import App from './app/App';import {AppProvider} from './state/AppState';import './styles/index.css';
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><HashRouter><AppProvider><App/></AppProvider></HashRouter></React.StrictMode>);
