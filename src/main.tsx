c32dc28a5ffe8f42121c37549f9be9e69d74bf1b
Vivek-k24 <105099587+Vivek-k24@users.noreply.github.com>
2026-07-30 18:59:03 -0500

    Load V3 design styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,15 @@
-import React from 'react';import ReactDOM from 'react-dom/client';import {HashRouter} from 'react-router-dom';import App from './app/App';import {AppProvider} from './state/AppState';import './styles/index.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><HashRouter><AppProvider><App/></AppProvider></HashRouter></React.StrictMode>);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {HashRouter} from 'react-router-dom';
+import App from './app/App';
+import {AppProvider} from './state/AppState';
+import './styles/index.css';
+import './styles/v3.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <AppProvider><App/></AppProvider>
+    </HashRouter>
+  </React.StrictMode>
+);
