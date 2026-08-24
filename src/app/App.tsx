2e0d9b8d194f914501c96d4cc87e766831c30330
Vivek-k24 <105099587+Vivek-k24@users.noreply.github.com>
2026-08-24 16:32:59 -0500

    Publish 6th generation Civic cooling parts stage
    * Add 6th generation Civic cooling parts data
    
    * Build 6th generation Civic parts workflow
    
    * Style 6th generation Civic workflow for desktop and mobile
    
    * Route PartGraph to 6th generation staged release

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,12 @@
 import {Route,Routes} from 'react-router-dom';
+import {PartGraphSixthGen} from '../pages/partgraph/PartGraphSixthGen';
 import {PartGraphStep2} from '../pages/partgraph/PartGraphStep2';
 
 export default function App(){
-  return <Routes><Route path="*" element={<PartGraphStep2/>}/></Routes>;
+  return (
+    <Routes>
+      <Route path="/8th-gen" element={<PartGraphStep2/>}/>
+      <Route path="*" element={<PartGraphSixthGen/>}/>
+    </Routes>
+  );
 }
