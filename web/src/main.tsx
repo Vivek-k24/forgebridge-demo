2c5bd686523f4f5b7c8fabf694efbeed0896849f
Vivek-k24 <105099587+Vivek-k24@users.noreply.github.com>
2026-08-27 22:14:28 -0500

    Block 5: install year wheel mouse support

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AuthGate from './AuthGate'
 import Block5Shell from './Block5Shell'
+import { installYearWheelInputSupport } from './year-wheel-input'
 import './app.css'
+
+installYearWheelInputSupport()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
