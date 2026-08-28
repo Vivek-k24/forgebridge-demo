30cebcd76723c0b1820faa82940e621953b1a383
Vivek-k24 <105099587+Vivek-k24@users.noreply.github.com>
2026-08-27 22:15:00 -0500

    UI blueprint: install year wheel mouse support

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AuthGate from './AuthGate'
 import BlueprintShell from './blueprint/BlueprintShell'
+import { installYearWheelInputSupport } from './year-wheel-input'
 import './app.css'
+
+installYearWheelInputSupport()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
