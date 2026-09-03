17d8aacdecdd8256bd0d1a24be94a2b2796486e8
Vivek-k24 <105099587+Vivek-k24@users.noreply.github.com>
2026-09-02 21:07:51 -0500

    Export unstructured collector interfaces

--- api/partgraph/collectors/__init__.py
+++ api/partgraph/collectors/__init__.py
@@ -9,16 +9,19 @@ from .staging import (
     stage_observation,
     start_ingestion_batch,
 )
+from .unstructured import SourceDocument, source_document_observation
 
 __all__ = [
     "CandidateType",
     "CollectedObservation",
     "EbayCatalogClient",
     "EbayCatalogError",
+    "SourceDocument",
     "VehicleApplication",
     "finish_ingestion_batch",
     "observation_dedupe_key",
     "raw_payload_sha256",
+    "source_document_observation",
     "stage_observation",
     "start_ingestion_batch",
 ]
