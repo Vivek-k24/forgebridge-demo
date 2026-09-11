95f3dc640e14338e868f2d4f86e8ac2a8de519ba
Vivek-k24 <105099587+Vivek-k24@users.noreply.github.com>
2026-09-11 17:43:34 -0500

    Remove obsolete static Pages preview path

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,20 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AuthGate from './AuthGate'
 import PartGraphShell from './PartGraphShell'
-import ProductionLaunch from './ProductionLaunch'
 import './app.css'
 import './light-panel-contrast.css'
 
-const app = <PartGraphShell />
-const directPagesPreview = import.meta.env.MODE === 'pages'
-  && window.location.hostname.endsWith('github.io')
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {directPagesPreview ? (
-      <ProductionLaunch />
-    ) : (
-      <AuthGate>{app}</AuthGate>
-    )}
+    <AuthGate>
+      <PartGraphShell />
+    </AuthGate>
   </StrictMode>,
 )
