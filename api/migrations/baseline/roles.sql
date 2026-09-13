2db52e2fcaeea13d849d7f36a5cbf980942d9e8f
Vivek-k24 <105099587+Vivek-k24@users.noreply.github.com>
2026-09-13 11:44:43 -0500

    Add schema baseline role bootstrap

---
+++ api/migrations/baseline/roles.sql
@@ -0,0 +1,16 @@
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'partgraph_collector') THEN
+        CREATE ROLE partgraph_collector NOLOGIN;
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'partgraph_app') THEN
+        CREATE ROLE partgraph_app NOLOGIN;
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'partgraph_reviewer') THEN
+        CREATE ROLE partgraph_reviewer NOLOGIN;
+    END IF;
+END
+$$;
+
+GRANT partgraph_app TO CURRENT_USER WITH INHERIT FALSE, SET TRUE;
+GRANT partgraph_reviewer TO CURRENT_USER WITH INHERIT FALSE, SET TRUE;
