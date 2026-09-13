6e073a1b1143ab06002e6f65291cb66bcb02cebe
Vivek-k24 <105099587+Vivek-k24@users.noreply.github.com>
2026-09-13 01:04:57 -0500

    Register offline app shell cache

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,12 +4,14 @@ import AuthGate from './AuthGate'
 import OfflineBootstrap from './OfflineBootstrap'
 import OfflineContinuity from './OfflineContinuity'
 import PartGraphShell from './PartGraphShell'
+import { registerPartGraphServiceWorker } from './service-worker'
 import { initializeUiPreferences } from './ui-preferences'
 import './app.css'
 import './light-panel-contrast.css'
 import './accessibility-ui.css'
 
 initializeUiPreferences()
+registerPartGraphServiceWorker()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
