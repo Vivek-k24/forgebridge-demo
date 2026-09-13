db576a94fb85b5dbb09bdb33c846b27bee3e3bab
Vivek-k24 <105099587+Vivek-k24@users.noreply.github.com>
2026-09-13 09:33:14 -0500

    Register physical relationship graph models

--- api/partgraph/orm_registry.py
+++ api/partgraph/orm_registry.py
@@ -16,6 +16,7 @@ import partgraph.knowledge.fitment  # noqa: F401
 import partgraph.knowledge.interchange  # noqa: F401
 import partgraph.knowledge.models  # noqa: F401
 import partgraph.knowledge.parts  # noqa: F401
+import partgraph.knowledge.relationships  # noqa: F401
 import partgraph.knowledge.vehicle_structure  # noqa: F401
 import partgraph.operator.models  # noqa: F401
 import partgraph.repair_experience.completion_models  # noqa: F401
